55791a16b86f95745dab319f08488b64d6b73d7b
Luka Giorgadze <l.giorgadze@bog.ge>
2016-12-01 16:42:06 +0400

    Removed MUI and Tap inject

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
@@ -7,8 +6,6 @@ import routes from './Routes'
 
 require('./css/cssreset.css');
 require('./css/main.css');
-
-injectTapEventPlugin();
 
 render(
 	<Router history={browserHistory} routes={routes} />,
